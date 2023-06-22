544e74610a7fdd929a27c41308531ace53e1a171
Felipe Carrera <f.carrera@duocuc.cl>
2023-06-21 23:33:29 -0400

    se agregó el estado y poder cambiarlo

--- core/serializers.py
+++ core/serializers.py
@@ -2,6 +2,7 @@
 # SERIALIZER -> VIEWSET -> URL
 from .models import *
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class TipoProductoSerializers(serializers.ModelSerializer):
     class Meta:
@@ -16,3 +17,13 @@ class ProductoSerializers(serializers.ModelSerializer):
         model = Producto
         fields = '__all__'
 
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class CompraSerializers(serializers.ModelSerializer):
+    usuario = UserSerializers(read_only = True)
+    class Meta:
+        model = Compra
+        fields = '__all__'
